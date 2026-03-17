2835d02b1bfeb7b42c547ce5cda5ac6206a6827d
e2500948 <rafikirossi@icloud.com>
2026-03-17 14:07:05 +0100

    ez

--- Tp1 R208/Exo1.py
+++ Tp1 R208/Exo1.py
@@ -1,14 +1,13 @@
 class Voitures():
-    def __init__(self, marque="Ferrari", modele="SF90_spider", annee=2010):
+    def __init__(self, marque, modele, annee):
         self.marque = marque
-        self.modele = modele
         self.annee = annee
+        self.modele = modele
 
-    # Étape 3 : Redéfinition de la méthode magique __str__ pour un affichage propre
     def __str__(self):
         return f"Valeurs des attributs de l'instance : {self.marque} {self.modele} {self.annee}"
 
-print("--- Étapes 1 à 4 : Création de base et test de __str__ ---")
+
 car = Voitures("Renault", "Clio", 2018)
 caisse = car.modele
 print(f"J'ai une {car.marque} {caisse} de {car.annee} !")
@@ -16,37 +15,18 @@ car.annee = 2020
 print(f"J'ai une {car.marque} {caisse} de {car.annee} !")
 print(car)
 
+car2 = Voitures("Peugeot", "208", 2021)
+print(car2)
 
-print("\n--- Étape 5 : 2ème instance ---")
-voiture2 = Voitures("Peugeot", "208", 2021)
-print(voiture2)
-
-
-print("\n--- Étape 7 : 3ème instance ---")
 ma_voiture = Voitures("Bugatti", "Veyron")
 print(ma_voiture)
 
-
-print("\n--- Étape 9 : 4ème instance ---")
 voiture4 = Voitures()
-print(voiture4)
+print("Voiture 4 :", voiture4)
 
-
-print("\n--- Étape 10 : 5ème instance ---")
 voiture5 = Voitures("F40")
-print(voiture5)
+print("Voiture 5 :", voiture5)
 
-
-print("\n--- Étape 11 : 6ème instance ---")
 voiture6 = Voitures(modele="296_GTS")
-print(voiture6)
+print("Voiture 6 :", voiture6)
 
-
-print("\n--- Étapes 12 à 14 : Introspection ---")
-# Étape 12
-print("Type (étape 12) :", type(voiture6))
-# Étape 13
-print("Vars (étape 13) :", vars(voiture6))
-# Étape 14
-print("Dir  (étape 14) :", dir(voiture6))
-zzz
